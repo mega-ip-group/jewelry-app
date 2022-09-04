7a15f016ef86e08cc9c361f0fbcd8527faac3649
Ehab Melhem <Melhemehab@gmail.com>
2022-09-04 23:34:17 +0300

    setup react router dom

--- app/src/Components/Genral/Header/HeaderList/HeaderList.js
+++ app/src/Components/Genral/Header/HeaderList/HeaderList.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import "./HeaderList.css"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./HeaderList.css";
 function HeaderList() {
+  let navigate = useNavigate();
+
+  const LoginHandler = () => {
+    navigate("/login");
+  };
   return (
-        <ul className='header__list'>
-        <li>home</li>
-        <li>Shop</li>
-        <li>Dashboard</li>
-        <li>Contact</li>
-        <li>Login</li>
-        </ul>
-  )
+    <ul className="header__list">
+      <li>home</li>
+      <li>Shop</li>
+      <li>Dashboard</li>
+      <li>Contact</li>
+      <li onClick={LoginHandler}>Login</li>
+    </ul>
+  );
 }
 
-export default HeaderList
+export default HeaderList;
