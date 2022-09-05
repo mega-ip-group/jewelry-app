8677e2f476486f63abe994bb01adf918d75725ec
megaIp <mhmdmsao188@gmail.com>
2022-09-05 23:13:24 +0300

    added the InfoSection

--- app/src/App.js
+++ app/src/App.js
@@ -10,11 +10,13 @@ import Service from "./Components/Genral/Service/Service";
 import Blog from "./Components/Genral/Blog/Blog";
 import TopView from "./Components/Genral/TopView/TopView";
 import MoreInfo from "./Components/Genral/MoreInfo/MoreInfo";
+import InfoSection from "./Components/Genral/InfoSection/InfoSection";
 function App() {
   function Home() {
     return (
       <React.Fragment>
         <Banner />
+        <InfoSection/>
         <Service/>
         <TopView/>
         <MoreInfo/>
---
+++ app/src/Components/Genral/InfoSection/InfoSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container } from '@mui/system';
+import img2 from '../../../images/halter.png';
+import img1 from '../../../images/shoes.png';
+import img3 from '../../../images/watches.png';
+import './InfoSection.css';
+
+const InfoSection = () => {
+    return (
+        <Container>
+            <div className='mt-5gy-2'>
+                <div >
+                    <img  src={img1} alt="" className='img1'/>
+                </div>
+                <div className='two_images'>
+                    <img  src={img2} alt="" />
+                    <img className='mt-4' src={img3} alt="" />
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default InfoSection;
