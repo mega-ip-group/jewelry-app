5d54f646503c986c2ef740b436f06461ce6e9a69
Ehab Melhem <Melhemehab@gmail.com>
2022-09-07 02:06:11 +0300

    add visa card

--- app/src/MainView/Cart/Dialog/Dialog.jsx
+++ app/src/MainView/Cart/Dialog/Dialog.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Card from "./Card";
 import PropTypes from "prop-types";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
@@ -13,7 +12,6 @@ import PersonIcon from "@mui/icons-material/Person";
 import AddIcon from "@mui/icons-material/Add";
 import Typography from "@mui/material/Typography";
 import { blue } from "@mui/material/colors";
-import { VisaCreditCard as VisaCard } from "react-fancy-visa-card";
 
 const emails = ["username@gmail.com", "user02@gmail.com"];
 
@@ -27,18 +25,39 @@ function SimpleDialog(props) {
   const handleListItemClick = (value) => {
     onClose(value);
   };
-  const pay = (e, data) => {
-    console.log(e);
-    console.log(data);
-  };
 
   return (
     <Dialog onClose={handleClose} open={open}>
-      <VisaCard
-        onSubmit={pay}
-        submitBtnTxt="Pay $350"
-        frontCardColor="linear-gradient(50deg, #f3c680, hsla(179,54%,76%,1))"
-      />
+      <DialogTitle>Set backup account</DialogTitle>
+      <List sx={{ pt: 0 }}>
+        {emails.map((email) => (
+          <ListItem
+            button
+            onClick={() => handleListItemClick(email)}
+            key={email}
+          >
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                <PersonIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={email} />
+          </ListItem>
+        ))}
+
+        <ListItem
+          autoFocus
+          button
+          onClick={() => handleListItemClick("addAccount")}
+        >
+          <ListItemAvatar>
+            <Avatar>
+              <AddIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Add account" />
+        </ListItem>
+      </List>
     </Dialog>
   );
 }
@@ -48,7 +67,8 @@ SimpleDialog.propTypes = {
   open: PropTypes.bool.isRequired,
   selectedValue: PropTypes.string.isRequired,
 };
-function Cart() {
+
+export default function SimpleDialogDemo() {
   const [open, setOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState(emails[1]);
 
@@ -62,18 +82,10 @@ function Cart() {
   };
 
   return (
-    <div className="cat">
-      <button onClick={handleClickOpen} class="buy__now">
-        Buy now
-      </button>
-      <label htmlFor="">total</label>
-      <div className="cart__container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open simple dialog
+      </Button>
       <SimpleDialog
         selectedValue={selectedValue}
         open={open}
@@ -82,5 +94,3 @@ function Cart() {
     </div>
   );
 }
-
-export default Cart;
