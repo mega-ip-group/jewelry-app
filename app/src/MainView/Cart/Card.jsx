63d27ac71781402ba40214b8819673a14a6f3cec
Ehab Melhem <Melhemehab@gmail.com>
2022-09-08 01:59:08 +0300

    add categories

--- app/src/MainView/Cart/Card.jsx
+++ app/src/MainView/Cart/Card.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./Card.css";
-function Card() {
+function Card({ url, description, price }) {
   return (
     <div className="card">
-      <img
-        src="https://mave.co.il/wp-content/uploads/2022/05/%D7%A2%D7%99%D7%A6%D7%95%D7%91-%D7%9C%D7%9C%D7%90-%D7%A9%D7%9D-2022-05-22T135427.605.png"
-        alt=""
-      />
+      <img src={url} alt="" />
       <div className="card__info">
-        <h1>this is the title</h1>
-        <p>description</p>
-        <p>100$</p>
+        <h1>{description}</h1>
+        <p>{description}</p>
+        <p>{price}$</p>
       </div>
     </div>
   );
